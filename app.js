62e13208b5f246d306212b7265aef9565014d5fc
Karl Kwon <newmkka@gmail.com>
2016-12-27 01:45:22 -0600

    express app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-var expreee = require('express');
+var express = require('express');
 var app = express();
+
+app.get('/', (req,res) => {
+  res.send("Hello web");
+})
+
+app.get('/login', (req,res) => {
+  res.send("<h1>Please login</h1>");
+})
+
+app.listen(3000, function() {
+  console.log("Connected 3000!");
+})
