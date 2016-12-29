528e0426a87e9ec369c7355586f7d43cafbfcefb
Karl Kwon <newmkka@gmail.com>
2016-12-28 18:51:32 -0600

    rewrite

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
+
+// var express = require('express');
+// var app = express();
+//
+// app.get('/', (req,res) => {
+//   res.send("Hello web");
+// })
+//
+// app.get('/login', (req,res) => {
+//   res.send("<h1>Please login</h1>");
+// })
+//
+// app.listen(3000, function() {
+//   console.log("Connected 3000!");
+// })
+
+
+
 var express = require('express');
 var app = express();
 
-app.get('/', (req,res) => {
-  res.send("Hello web");
+app.listen(3500, ()=>{
+  console.log("Connected 3500!!!");
 })
 
-app.get('/login', (req,res) => {
-  res.send("<h1>Please login</h1>");
+app.get('/hello', (req, res)=>{
+    res.send('<h1>hello</h1>')
 })
 
-app.listen(3000, function() {
-  console.log("Connected 3000!");
+app.get('/', (req, res)=>{
+  res.send('Main Entry Point in nodejs');
 })
