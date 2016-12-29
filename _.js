c9e2d11c0714107289539f6427b91650f9bd0716
Karl Kwon <newmkka@gmail.com>
2016-12-28 18:56:29 -0600

    push

--- _.js
+++
@@ -1,3 +0,0 @@
-const _ = require('underscore');
-var arr = [1,2,3,4,5];
-console.log(_.first(arr), _.last(arr));
--- callback.js
+++ callback.js
@@ -1,10 +1,23 @@
-var arr = [3,1,4,2,5];
+// var arr = [3,1,4,2,5];
+// console.log(arr);
+// console.log(arr.sort());
+// console.log(arr.sort(function(a,b){return b-a}));
+//
+// function _desc (a, b) {
+//   return b-a;
+// };
+//
+// console.log(arr.sort(_desc));
+
+
+var arr = [2,4,6,8,3,5,7,1];
 console.log(arr);
 console.log(arr.sort());
-console.log(arr.sort(function(a,b){return b-a}));
+console.log(arr.sort(function(a, b){return b-a;}));
 
-function _desc (a, b) {
+function desc(a, b) {
+  console.log('call back function:', a, b);
   return b-a;
-};
+}
 
-console.log(arr.sort(_desc));
+console.log(arr.sort(desc));
