09fd5713214ff504260c00f7fc4edca62d7c7672
Karl Kwon <newmkka@gmail.com>
2016-12-28 19:25:11 -0600

    node variable

--- app.js
+++ app.js
@@ -10,10 +10,31 @@ app.get('/', (req, res)=>{
   res.send('Main Entry Point in nodejs');
 });
 
+app.get('/dynamic', function(req, res){
+  var lis = ''
+  for( var i=0; i<5; i++ ){
+    lis = lis + '<li>Coding</li>';
+  }
+  var output = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      Hello, dynamic!
+      ${lis}
+    </body>
+  </html>
+`;
+  res.send(output);
+});
+
 app.get('/hello', (req, res)=>{
     res.send('<h1>hello</h1>');
 });
 
 app.listen(3000, ()=>{
   console.log("Connected 3000!!!");
-})
+});
