b6d5e6c18a04233fa6654ef6188bca4641681a42
Karl Kwon <newmkka@gmail.com>
2016-12-29 17:20:49 -0600

    post method with body-parser (middleware)

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body_parser');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -8,6 +11,13 @@ app.locals.pretty = true;
 app.use(express.static('public'));
 app.get('/form', function(req, res){
   res.render('form');
+})
+app.post('/form_receiver', (req, res)=>{
+  var title = req.body.title;
+  var desc = req.body.desc;
+  res.send('Hello, POST: '+title+', ' +desc);
+
+  // res.json(req.body);
 })
 app.get('/form_receiver', function(req,res){
   var title = req.query.title;
