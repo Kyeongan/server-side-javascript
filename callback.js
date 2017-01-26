8158fcc8769b8f1a4c85fe8cdc36821a01ad5225
Karl Kwon <newmkka@gmail.com>
2017-01-26 12:07:52 -0600

    call back function

--- callback.js
+++ callback.js
@@ -10,14 +10,28 @@
 // console.log(arr.sort(_desc));
 
 
-var arr = [2,4,6,8,3,5,7,1];
-console.log(arr);
-console.log(arr.sort());
-console.log(arr.sort(function(a, b){return b-a;}));
+// var arr = [2,4,6,8,3,5,7,1];
+// console.log(arr);
+// console.log(arr.sort());
+// console.log(arr.sort(function(a, b){return b-a;}));
+
+// function desc(a, b) {
+//   console.log('call back function:', a, b);
+//   return b-a;
+// }
+
+// console.log(arr.sort(desc));
+
+
+
+
+var arr2 = [1,2,4,5,6,3];
+console.log(arr2);
+console.log(arr2.sort());
+console.log(arr2.sort(function(a, b){return b-a;}));
 
 function desc(a, b) {
-  console.log('call back function:', a, b);
-  return b-a;
+	return b-a;
 }
 
-console.log(arr.sort(desc));
+console.log(arr2.sort(desc));
